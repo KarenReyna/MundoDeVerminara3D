91b8fce3024bfc57871a1a42e635970b00d31a38
Karen Reyna <karen.reyna95@gmail.com>
2016-03-29 20:46:26 -0600

    Texturas
    Se agregaron las texturas del fondo

--- Windows/main.cpp
+++ Windows/main.cpp
@@ -1,7 +1,7 @@
 /*
  *   Ana Karen Reyna Rivas   A01280310
  *   Iker Arbulu Lozano      A01190690
- *   Segundo Avance: Proyecto El Mundo de Verminara
+ *   Tercer Avance: Proyecto El Mundo de Verminara
  */
 
 #ifdef __APPLE__
@@ -21,6 +21,7 @@
 #include "glm.h"
 #include "imageloader.h"
 #include "imageBMP.h"
+#include <assert.h>
 
 using namespace std;
 
@@ -35,12 +36,61 @@ bool inicio = true, pausado, comenzado, reiniciar, terminado, jugando;
 bool autores, instrucciones,historia;
 
 // Para objetos 3D
-GLuint texName[36];                                     // Texturas
+static GLuint texName[36];                                    // Texturas
 GLMmodel model[6];
 string fullPath = __FILE__;
 GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };      //Puntual
 
 int angulo=-1;                                          // Animación de objetos
+
+
+//Makes the image into a texture, and returns the id of the texture
+void loadTexture(Image* image,int k)
+{
+
+    glBindTexture(GL_TEXTURE_2D, texName[k]); //Tell OpenGL which texture to edit
+
+    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+    //    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+
+    //Map the image to the texture
+    glTexImage2D(GL_TEXTURE_2D,                //Always GL_TEXTURE_2D
+                 0,                            //0 for now
+                 GL_RGB,                       //Format OpenGL uses for image
+                 image->width, image->height,  //Width and height
+                 0,                            //The border of the image
+                 GL_RGB, //GL_RGB, because pixels are stored in RGB format
+                 GL_UNSIGNED_BYTE, //GL_UNSIGNED_BYTE, because pixels are stored
+                 //as unsigned numbers
+                 image->pixels);               //The actual pixel data
+}
+
+void initRendering()
+{
+    int i=0;
+    /*glEnable(GL_DEPTH_TEST);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE); ///Users/mariaroque/Imagenes*/
+    // glEnable(GL_COLOR_MATERIAL);
+    glGenTextures(6, texName); //Make room for our texture
+    Image* image = loadBMP("C:/Users/karen_000/Dropbox/6 Semestre/Gráficas/MundoDeVerminara3D/Windows/imagenes/MenuResized.bmp");
+    loadTexture(image,i++);
+    image = loadBMP("C:/Users/karen_000/Dropbox/6 Semestre/Gráficas/MundoDeVerminara3D/Windows/imagenes/AutoresResized.bmp");
+    loadTexture(image,i++);
+    image = loadBMP("C:/Users/karen_000/Dropbox/6 Semestre/Gráficas/MundoDeVerminara3D/Windows/imagenes/InstruccionesResized.bmp");
+    loadTexture(image,i++);
+    image = loadBMP("C:/Users/karen_000/Dropbox/6 Semestre/Gráficas/MundoDeVerminara3D/Windows/imagenes/HistoriaResized.bmp");
+    loadTexture(image,i++);
+    delete image;
+}
+
 
 // Timer para que los objetos roten
 static void timer(int i){
@@ -51,7 +101,6 @@ static void timer(int i){
 
 static void objeto3D(){
     glmDraw(&model[0], GLM_TEXTURE| GLM_COLOR | GLM_FLAT);
-
 }
 
 // Desplegar texto en tamaño pequeño
@@ -115,11 +164,36 @@ static void dibujaBaseAutores(){
     glTranslatef(-50,0,0);
 }
 
+// Función para cargar la imagen textura del fondo
+static void cargarImagenFondo(int indice){
+    // Background Image Texture
+    glPushMatrix();
+        glTranslatef(-300,-300,-45);
+        glPointSize(1);
+        glLineWidth(3);
+        glBindTexture(GL_TEXTURE_2D, texName[indice]);
+        glBegin(GL_QUADS);
+            glColor4ub(255, 255, 255,255);       // Color
+            glTexCoord2f(1.0f, 0.0f);
+            glVertex2f(600, 0);                 // v0
+            glTexCoord2f(1.0f, 1.0f);
+            glVertex2f(600, 600);               // v1
+            glTexCoord2f(0.0f, 1.0f);
+            glVertex2f(0, 600);                 // v2
+            glTexCoord2f(0.0f, 0.0f);
+            glVertex2f(0, 0);                   // v3
+        glEnd();
+    glPopMatrix();
+}
+
 // Desplegado en pantalla principal
 static void pantallaInicial(){
+    // Cargar la imagen textura del fondo
+    cargarImagenFondo(0);
+
     // Botón Autores
     glPushMatrix();
-        glTranslatef(-((ancho/2)-50),(largo/4)+80,-45);
+        glTranslatef(((ancho/2)-60),(largo/4)+80,-45);
         glRotatef(angulo,0,1,0);
         glScalef(0.8,1,1);
         glTranslatef(-35,0,0);
@@ -128,361 +202,41 @@ static void pantallaInicial(){
         letreroLetraPequena("A - Autores");
     glPopMatrix();
 
-    // Titulo
-    glPushMatrix();
-        glColor3f(0,0,0);
-        glTranslatef(-180,220,-45);
-        glRotatef(-30,1,1,0);
-        glScalef(2.5,1,2);
-        letrero("El Mundo");
-        glTranslatef(45,0,-50);
-        letrero("de Verminara");
-    glPopMatrix();
-
-    // Separador
-    glPushMatrix();
-        glColor3f(0.098,0.098,0.439);
-        glTranslatef(0,0,2);
-        glRotatef(1,0,0,1.5);
-        glLineWidth(1);
-        glTranslatef(0,0,1);
-        for(int i=0;i<3;i++){
-            glBegin(GL_LINES);
-            glVertex2f(-(ancho/2),((largo/4)-(1.5*i)));
-            glVertex2f((ancho/2),((largo/4 + 20)-(1.5*i)));
-            glEnd();
-        }
-    glPopMatrix();
-
     // Objeto 3D
-    glPushMatrix();
+    /*glPushMatrix();
         glTranslatef((ancho/4),100,-45);
         glScalef(40,40,15);
         glRotatef(angulo,0,1,0);
         objeto3D();
-    glPopMatrix();
-
-    // Instrucciones
-    glPushMatrix();
-        glColor3f(0,1,0);
-        glTranslatef(-320,0,-45);
-        glRotatef(-30,1,1,0);
-        glPushMatrix();
-            glScalef(2.5,1,2);
-            glRectf(20, 50, 90, 90);
-        glPopMatrix();
-        glTranslatef(0,0,20);
-        letreroMenu("I - Instrucciones");
-    glPopMatrix();
-
-    // Historia
-    glPushMatrix();
-        glColor3f(1,0.27,0);
-        glTranslatef(-320,-50,-45);
-        glRotatef(-30,1,1,0);
-        glPushMatrix();
-            glScalef(2.5,1,2);
-            glRectf(20, 50, 90, 90);
-        glPopMatrix();
-        glTranslatef(0,0,20);
-        letreroMenu("H - Historia");
-    glPopMatrix();
-
-    // Comenzar partida
-    glPushMatrix();
-        glColor3f(0,0,0.545);
-        glTranslatef(-320,-100,-45);
-        glRotatef(-30,1,1,0);
-        glPushMatrix();
-            glScalef(2.5,1,2);
-            glRectf(20, 50, 90, 90);
-        glPopMatrix();
-        glTranslatef(0,0,20);
-        letreroMenu("C - Comenzar");
-    glPopMatrix();
-
-    // Pausar partida
-    glPushMatrix();
-        glColor3f(0.501, 0, 0);
-        glTranslatef(-320,-150,-45);
-        glRotatef(-30,1,1,0);
-        glPushMatrix();
-            glScalef(2.5,1,2);
-            glRectf(20, 50, 90, 90);
-        glPopMatrix();
-        glTranslatef(0,0,20);
-        letreroMenu("P - Pausar");
-    glPopMatrix();
-
-    // Reiniciar partida
-    glPushMatrix();
-        glColor3f(1,0,1);
-        glTranslatef(-320,-200,-45);
-        glRotatef(-30,1,1,0);
-        glPushMatrix();
-            glScalef(2.5,1,2);
-            glRectf(20, 50, 90, 90);
-        glPopMatrix();
-        glTranslatef(0,0,20);
-        letreroMenu("R - Reiniciar");
-    glPopMatrix();
-
-    // Quitar música
-    glPushMatrix();
-        glColor3f(0,0.749,1);
-        glTranslatef(-320,-250,-45);
-        glRotatef(-30,1,1,0);
-        glPushMatrix();
-            glScalef(2.5,1,2);
-            glRectf(20, 50, 90, 90);
-        glPopMatrix();
-        glTranslatef(0,0,20);
-        letreroMenu("M - Quitar musica");
-    glPopMatrix();
-
-    // Ver Niveles
-    glPushMatrix();
-        glColor3f(1,0.843,0);
-        glTranslatef(-320,-300,-45);
-        glRotatef(-30,1,1,0);
-        glPushMatrix();
-            glScalef(2.5,1,2);
-            glRectf(20, 50, 90, 90);
-        glPopMatrix();
-        glTranslatef(0,0,20);
-        letreroMenu("N - Ver Niveles");
-    glPopMatrix();
+    glPopMatrix();*/
 }
 
 // Despliega pantalla de autores
 static void pantallaAutores(){
-    // Titulo
-    glPushMatrix();
-        glColor3f(0,0,0);
-        glTranslatef(-180,220,-45);
-        glRotatef(-30,1,1,0);
-        glScalef(2.5,1,2);
-        letrero("Autores");
-    glPopMatrix();
-
-    // Separador
-    glPushMatrix();
-        glColor3f(0.098,0.098,0.439);
-        glTranslatef(0,0,2);
-        glRotatef(1,0,0,1.5);
-        glLineWidth(1);
-        glTranslatef(0,0,1);
-        for(int i=0;i<3;i++){
-            glBegin(GL_LINES);
-            glVertex2f(-(ancho/2),((largo/4)-(1.5*i)));
-            glVertex2f((ancho/2),((largo/4 + 20)-(1.5*i)));
-            glEnd();
-        }
-    glPopMatrix();
-
-    char nombre1[200]="";
-    char nombre2[200]="";
-    sprintf(nombre1, "%s", "Ana Karen Reyna Rivas   Matricula: A01280310");
-    sprintf(nombre2, "%s", "Iker Arbulu Lozano   Matricula: A01190690");
-    int xRaster = 5;
-    // Dibuja autores
-    glRasterPos2i(-ancho * .4, largo *.05);
-    for (GLint k = 0; nombre1[k]!='\0'; k++)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, nombre1[k]);
-    }
-    glRasterPos2i(-ancho * .4, largo *-.05);
-    for (GLint k = 0; nombre2[k]!='\0'; k++)
-    {
-        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, nombre2[k]);
-    }
+    // Cargar la imagen textura del fondo
+    cargarImagenFondo(1);
 
     // Botón Quitar autores
-    glPushMatrix();
+    /*glPushMatrix();
         glTranslatef(0,-150,-45);
         glRotatef(angulo,0,1,0);
         glScalef(0.8,1,1);
         dibujaBaseAutores();
         glTranslatef(0,0,20);
         letreroLetraPequena("A - Home");
-    glPopMatrix();
+    glPopMatrix();*/
 }
 
 // Despliega pantalla de instrucciones
 static void pantallaInstrucciones(){
-    // Titulo
-    glPushMatrix();
-        glColor3f(0,0,0);
-        glTranslatef(-180,220,-100);
-        glRotatef(-30,1,1,0);
-        glScalef(2.5,1,2);
-        letrero("Instrucciones");
-    glPopMatrix();
-
-    // Separador
-    glPushMatrix();
-        glColor3f(0.098,0.098,0.439);
-        glTranslatef(0,0,2);
-        glRotatef(1,0,0,1.5);
-        glLineWidth(1);
-        glTranslatef(0,0,1);
-        for(int i=0;i<3;i++){
-            glBegin(GL_LINES);
-            glVertex2f(-(ancho/2),((largo/4)-(1.5*i)));
-            glVertex2f((ancho/2),((largo/4 + 20)-(1.5*i)));
-            glEnd();
-        }
-    glPopMatrix();
-
-    // Texto
-  glPushMatrix();
-    glColor3f(0, 0, 0);
-    char lineaInstruccion[200]="";
-    float yRaster = largo*.17;
-    sprintf(lineaInstruccion, "%s", "El juego consiste de varios retos que debes superar para mantener la");
-    glRasterPos2f(-ancho * .5, yRaster);
-    for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-    {
-      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-    }
-    yRaster-=25;
-    sprintf(lineaInstruccion, "%s", "salud de tu avatar, al ir cumpliendo con los retos que te pone el");
-    glRasterPos2f(-ancho * .5, yRaster);
-    for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-    {
-      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-    }
-    yRaster-=25;
-    sprintf(lineaInstruccion, "%s", "mundo de verminara la velocidad del juego ira avanzando.");
-    glRasterPos2f(-ancho * .5, yRaster);
-    for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-    {
-      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-    }
-    yRaster-=25;
-    sprintf(lineaInstruccion, "%s", "Una vez cumpliendo cada reto iras subiendo de nivel que se sumara a");
-    glRasterPos2f(-ancho * .5, yRaster);
-    for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-    {
-      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-    }
-    yRaster-=25;
-    sprintf(lineaInstruccion, "%s", "tu puntaje final.");
-    glRasterPos2f(-ancho * .5, yRaster);
-    for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-    {
-      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-    }
-    yRaster-=25;
-    sprintf(lineaInstruccion, "%s", "Cada juego tendra las instrucciones a seguir en la parte baja de la");
-    glRasterPos2f(-ancho * .5, yRaster);
-    for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-    {
-      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-    }
-    yRaster-=25;
-    sprintf(lineaInstruccion, "%s", "pantalla, demuestra quien es el mejor dominando el mundo de verminara.");
-    glRasterPos2f(-ancho * .5, yRaster);
-    for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-    {
-      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-    }
-    yRaster-=25;
-    //
-  glPopMatrix();
-
+    // Cargar la imagen textura del fondo
+    cargarImagenFondo(2);
 }
 
 // Despliega pantalla de historia
 static void pantallaHistoria(){
-  // Titulo
-  glPushMatrix();
-  glColor3f(0,0,0);
-  glTranslatef(-180,220,-100);
-  glRotatef(-30,1,1,0);
-  glScalef(2.5,1,2);
-  letrero("Historia");
-  glPopMatrix();
-
-  // Separador
-  glPushMatrix();
-  glColor3f(0.098,0.098,0.439);
-  glTranslatef(0,0,2);
-  glRotatef(1,0,0,1.5);
-  glLineWidth(1);
-  glTranslatef(0,0,1);
-  for(int i=0;i<3;i++){
-    glBegin(GL_LINES);
-    glVertex2f(-(ancho/2),((largo/4)-(1.5*i)));
-    glVertex2f((ancho/2),((largo/4 + 20)-(1.5*i)));
-    glEnd();
-  }
-  glPopMatrix();
-
-  // Texto
-  glPushMatrix();
-  glColor3f(0, 0, 0);
-  char lineaInstruccion[200]="";
-  float yRaster = largo*.17;
-  sprintf(lineaInstruccion, "%s", "Verminara es un mundo que cuenta con ciudadanos irresponsables y ");
-  glRasterPos2f(-ancho * .5, yRaster);
-  for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-  {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-  }
-  yRaster-=25;
-  sprintf(lineaInstruccion, "%s", "necesita de la ayuda de un conocedor responsable para poder ");
-  glRasterPos2f(-ancho * .5, yRaster);
-  for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-  {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-  }
-  yRaster-=25;
-  sprintf(lineaInstruccion, "%s", "rescatarlos de su fallas, ahi es donde entra el jugador.");
-  glRasterPos2f(-ancho * .5, yRaster);
-  for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-  {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-  }
-  yRaster-=25;
-  sprintf(lineaInstruccion, "%s", "El jugador viene a ayudar con diversos problemas del dia a");
-  glRasterPos2f(-ancho * .5, yRaster);
-  for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-  {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-  }
-  yRaster-=25;
-  sprintf(lineaInstruccion, "%s", "dia a los habitantes que pueden ser resueltos por simples");
-  glRasterPos2f(-ancho * .5, yRaster);
-  for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-  {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-  }
-  yRaster-=25;
-  sprintf(lineaInstruccion, "%s", "acciones. Para esto el jugador debe de contar con su");
-  glRasterPos2f(-ancho * .5, yRaster);
-  for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-  {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-  }
-  yRaster-=25;
-  sprintf(lineaInstruccion, "%s", "habilidad para rapidamente ayudar a todos los habitantes");
-  glRasterPos2f(-ancho * .5, yRaster);
-  for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-  {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-  }
-  yRaster-=25;
-  sprintf(lineaInstruccion, "%s", "de Verminara.");
-  glRasterPos2f(-ancho * .5, yRaster);
-  for (GLint k = 0; lineaInstruccion[k]!='\0'; k++)
-  {
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, lineaInstruccion[k]);
-  }
-  yRaster-=25;
-  //
-  glPopMatrix();
-
+    // Cargar la imagen textura del fondo
+    cargarImagenFondo(3);
 }
 
 // Toma la ubicación del proyecto
@@ -509,6 +263,7 @@ static void myDisplay(void)
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);     // Activa profundidad
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glEnable(GL_TEXTURE_2D);
 
     if(jugando){
 
@@ -536,7 +291,7 @@ void init(){
 
     // Inicializar Imagenes
     int i = 0;
-    getParentPath();
+
     glEnable(GL_NORMALIZE);
 
     // Objetos 3D
@@ -594,7 +349,9 @@ int main(int argc, char ** argv)
     glutInitWindowSize(ancho, largo);
     glutInitWindowPosition(0, 0);                           // Create window
     glutCreateWindow("El Mundo de Verminara");
+    getParentPath();
     init();
+    initRendering();
     glutDisplayFunc(myDisplay);
     glutKeyboardFunc(myKeyboard);
     glutReshapeFunc(reshape);
